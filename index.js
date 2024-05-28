0d4ceca5f76dfb5237cca5beeddb1dfd57f98c36
Barkov <sa_bar@mail.ru>
2024-05-28 12:04:58 +0300

    Сделал адаптив лендоса

---
+++ index.js
@@ -0,0 +1,54 @@
+const showOne = () => {
+    const forumOne = document.getElementById('one')
+    if (forumOne.style.display === 'none') {
+        forumOne.style.display = 'block'
+    } else {
+        forumOne.style.display = 'none'
+    }
+}
+
+const showTwo = () => {
+    const forumTwo = document.getElementById('two')
+    if (forumTwo.style.display === 'none') {
+        forumTwo.style.display = 'block'
+    } else {
+        forumTwo.style.display = 'none'
+    }
+}
+
+const showThree = () => {
+    const forumThree = document.getElementById('three')
+    if (forumThree.style.display === 'none') {
+        forumThree.style.display = 'block'
+    } else {
+        forumThree.style.display = 'none'
+    }
+}
+
+const showFour = () => {
+    const forumFour = document.getElementById('four')
+    if (forumFour.style.display === 'none') {
+        forumFour.style.display = 'block'
+    } else {
+        forumFour.style.display = 'none'
+    }
+}
+
+const showFive = () => {
+    const forumFive = document.getElementById('five')
+    if (forumFive.style.display === 'none') {
+        forumFive.style.display = 'block'
+    } else {
+        forumFive.style.display = 'none'
+    }
+}
+
+const showSix = () => {
+    const forumSix = document.getElementById('six')
+    if (forumSix.style.display === 'none') {
+        forumSix.style.display = 'block'
+    } else {
+        forumSix.style.display = 'none'
+    }
+}
+
