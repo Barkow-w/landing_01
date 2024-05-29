f69c0861ad423d1ca7e39e6db7b2c1d27518b49b
Barkov <sa_bar@mail.ru>
2024-05-29 18:23:48 +0300

    Исправил баги лендоса

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
+
+const back = () => {
+    let pageOne = document.getElementById('pageOne');
+    let pageTwo = document.getElementById('pageTwo');
+
+    if (pageTwo.style.display === 'flex') {
+        pageOne.style.display = 'flex'
+        pageTwo.style.display = 'none'
+    }
+}
+
+const next = () => {
+    let pageOne = document.getElementById('pageOne');
+    let pageTwo = document.getElementById('pageTwo');
+
+    if (pageTwo.style.display === 'none') {
+        pageOne.style.display = 'none'
+        pageTwo.style.display = 'flex'
+    }
+}
+
+const advantages = () => {
+    let section = document.getElementById('articles');
+    section.scrollIntoView({behavior: 'smooth'})
+}
+
+const comments = () => {
+    let section = document.getElementById('comments');
+    section.scrollIntoView({behavior: 'smooth'})
+}
+
+const partner = () => {
+    let section = document.getElementById('partner');
+    section.scrollIntoView({behavior: 'smooth'})
+}
+
+const forum = () => {
+    let section = document.getElementById('forum');
+    section.scrollIntoView({behavior: 'smooth'})
+}
+
+
+
+const play = () => {
+    const video = document.getElementById('video');
+    video.style.background = 'none'
+}
+
+
 const showOne = () => {
     const forumOne = document.getElementById('one')
     if (forumOne.style.display === 'none') {
@@ -66,3 +115,18 @@ const showMenu = () => {
         open.style.display = 'block'
     }
 }
+
+const pushForm = () => {
+    let nameModal = document.getElementById('name');
+    let telModal = document.getElementById('tel');
+
+    if (nameModal.value && telModal.value) {
+        console.log(nameModal.value, telModal.value)
+
+        nameModal.value = ''
+        telModal.value = ''
+    }
+}
+
+
+
