40b8b33e713fc4f99790cb06aa0ea5b8a2b9465a
Barkov <sa_bar@mail.ru>
2024-07-15 23:37:35 +0300

    Исправил замечаний и удалил лишнюю папку

--- index.js
+++ index.js
@@ -158,22 +158,7 @@ const pushForm = () => {
 const phoneMask = document.getElementById('tel');
 Inputmask({ mask: '+7 (999) 999-99-99', showMaskOnHover: false}).mask(phoneMask);
 
-$(document).ready(function () {
-    $('#image-list .image-list').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        cssEase: 'linear'
-    });
-    $('#catalog-list-1 .image-list').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        cssEase: 'linear'
-    });
-});
+
 
 
 
