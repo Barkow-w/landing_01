401d13a0983dd2ed83ba2fec5060fd80366a536e
Barkov <sa_bar@mail.ru>
2024-07-19 18:19:12 +0300

    Исправил фалы

--- index.js
+++ index.js
@@ -155,9 +155,6 @@ const pushForm = () => {
 
 }
 
-const phoneMask = document.getElementById('tel');
-Inputmask({ mask: '+7 (999) 999-99-99', showMaskOnHover: false}).mask(phoneMask);
-
 $(document).ready(function () {
     $('#image-list .image-list').slick({
         dots: true,
@@ -175,7 +172,8 @@ $(document).ready(function () {
     });
 });
 
-
+const phoneMask = document.getElementById('tel');
+Inputmask({ mask: '+7 (999) 999-99-99', showMaskOnHover: false}).mask(phoneMask);
 
 
 
